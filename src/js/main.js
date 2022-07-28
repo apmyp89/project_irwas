c8c458acf91d9c20ace0c68787e3a435d4d72851
Artur <apmyp89@ya.ru>
2022-07-28 22:35:57 +0300

    Create Tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 import "./slider";
 import modals from "./modals/modals";
+import tabs from "./modals/tabs";
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     modals();
+//     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+// });
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+    tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
 
-console.log('hi');
--- src/js/modals/modals.js
+++ src/js/modals/modals.js
@@ -14,35 +14,29 @@ const modals = () => {
                       // document.body.classList.add('modal-open');        
                 });
             });
-
-
-                close.addEventListener('click', () => {
+            close.addEventListener('click', () => {
                 modal.style.display = "none";
                 document.body.style.overflow = "";    
                  // document.body.classList.remove('modal-open');   
                 });
-
-                modal.addEventListener('click', (e) => {
+            modal.addEventListener('click', (e) => {
                     if (e.target === modal) {
                  modal.style.display = "none";
                  document.body.style.overflow = ""; 
                   // document.body.classList.remove('modal-open');   
-
                     }
                 });
         }
-
         function showModalByTime(selector, time) {
             setTimeout(() => {
                 document.querySelector(selector).style.display = 'block';
                 document.body.style.overflow = "hidden";
             }, time);
         }
-
         bindModal('.popup_engineer_btn', 
         '.popup_engineer', '.popup_engineer .popup_close');
         bindModal('.phone_link', '.popup', '.popup .popup_close');
-        showModalByTime('.popup', 3000);
+       // showModalByTime('.popup', 60000);
 };
 
 export default modals;
